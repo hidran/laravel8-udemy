99df115b53625030d320092f86e88ad2abf13787
hidran@gmail.com <hidran@gmail.com>
2020-10-02 15:34:59 +0200

    adds staff view

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,14 +4,58 @@ namespace App\Http\Controllers;
 
 class PageController extends Controller
 {
+    protected $data = [
+        [
+            'name' => 'Hidran',
+            'lastname' => 'SArias'
+
+        ],
+        [
+            'name' => 'James',
+            'lastname' => 'Blond'
+
+        ],
+        [
+            'name' => 'Harry',
+            'lastname' => 'Plotter'
+
+        ]
+    ];
+
     public function about()
     {
-
-      return view('about');
+        // $view = app('view');
+        //return $view('about');
+        return view('about');
+        // return   View::make('about');
     }
+
     public function blog()
     {
-
+        // $view = app('view');
+        //return $view('about');
         return view('blog');
+        // return   View::make('about');
+    }
+
+    public function staff()
+    {
+
+        /*     return view('staff',
+                 [
+                     'title' => 'Our staff',
+                     'staff' => $this->data
+                 ]
+             );
+
+             return view('staff')
+                 ->with('staff' , $this->data)
+                 ->with('title','Our Staff');
+        */
+        //return view('staff')->withStaff($this->data)->withTitle('Our staff');
+
+        $staff = $this->data;
+        $title = 'OUR STAFF';
+        return view('staff', compact('title','staff'));
     }
 }
--- routes/page.php
+++ routes/page.php
@@ -2,3 +2,4 @@
 use App\Http\Controllers\PageController;
 Route::get('/about', [PageController::class ,'about']);
 Route::get('/blog', [PageController::class ,'blog']);
+Route::get('/staff', [PageController::class ,'staff']);
