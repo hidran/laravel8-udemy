56c823d02d393a9390f190b5d2e35b16513618de
hidran@gmail.com <hidran@gmail.com>
2020-10-03 00:30:53 +0200

    adds albums and photostable migration

---
+++ database/migrations/2020_10_02_171316_create_album_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->string('album_name',128)->unique()->nullable(false);
+            $table->string('album_thumb');
+            $table->text('description')->nullable();
+            $table->foreignId('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
