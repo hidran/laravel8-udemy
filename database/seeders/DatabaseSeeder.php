aace5725afd332d36f66d5433b00568bd9c38452
hidran@gmail.com <hidran@gmail.com>
2020-10-05 00:12:36 +0200

    adds User Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Database\Seeders\UserSeeder;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        $this->call(UserSeeder::class);
     }
 }
