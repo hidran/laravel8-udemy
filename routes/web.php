ad6b6dabd7a32c7eea2c65f3760cbe4c08f0f845
hidran@gmail.com <hidran@gmail.com>
2020-10-11 17:08:50 +0200

    adds query builder joins

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,16 @@ Route::get('/albums/{album}', 'App\Http\Controllers\AlbumsController@show');
 Route::get('/albums/{album}', [AlbumsController::class, 'show']);
 
 
+Route::get('usersnoalbums' , function(){
+    $usersnoalbum = DB::table('users  as u')
+        ->leftJoin('albums as a', 'u.id','a.user_id')
+        ->select('u.id','email','name','album_name')->
+        whereRaw('album_name is null')
+        ->get();
+    $usersnoalbum = DB::table('users  as u')
+
+        ->select('u.id','email','name')->
+        whereRaw( ' EXISTS (SELECT user_id from albums where user_id= u.id)')
+        ->get();
+    return $usersnoalbum;
+});
