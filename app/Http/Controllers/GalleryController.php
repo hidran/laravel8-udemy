d1a9c41be7e586e97dac7b2f81a85f7251ee0da6
Hidran Arias <hidran@gmail.com>
2021-07-24 00:40:49 +0200

    shows albums images

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Album;
+use App\Models\Photo;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
@@ -10,5 +11,12 @@ class GalleryController extends Controller
     public function index()
     {
         return view('gallery.albums')->with('albums', Album::latest()->paginate(50));
+    }
+
+    public function showAlbumImages($album)
+    {
+       return view('gallery.images')->with('images',
+           Photo::whereAlbumId($album)->paginate(10)
+       );
     }
 }
