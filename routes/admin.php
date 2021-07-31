c361714955d8940977893d71be5cf981d0aaf667
Hidran Arias <hidran@gmail.com>
2021-08-01 01:53:35 +0200

    shows users by using yajra ajax datable

--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\AdminUsersController;
 
 Route::resource('users', AdminUsersController::class);
 Route::view('/', 'templates/admin')->name('admin');
-Route::get('getUsers', [AdminUsersController::class, 'getUsers']);
+Route::get('getUsers', [AdminUsersController::class, 'getUsers'])->name('admin.getUsers');
 
 Route::get('/dashboard', function () {
     return 'Admin DashBoard';
